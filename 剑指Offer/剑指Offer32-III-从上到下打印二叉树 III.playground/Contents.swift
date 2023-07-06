aaa11dc04e95eb5a4e27bd24bb53a0744b4cb586
Huang Runhua <45262463+HuangRunHua@users.noreply.github.com>
2023-07-06 13:09:08 +0800

    Add Code

--- 剑指Offer/剑指Offer32-III-从上到下打印二叉树 III.playground/Contents.swift
+++ 剑指Offer/剑指Offer32-III-从上到下打印二叉树 III.playground/Contents.swift
@@ -12,16 +12,46 @@ public class TreeNode {
 }
 
 class Solution {
-    /// 执行用时：8 ms, 在所有 Swift 提交中击败了 90.00% 的用户
-    /// 内存消耗：13.9 MB, 在所有 Swift 提交中击败了 88.00% 的用户
+    /// 执行用时：12ms, 在所有 Swift 提交中击败了 55.00% 的用户
+    /// 内存消耗：14.1 MB, 在所有 Swift 提交中击败了 22.5% 的用户
     /// 通过测试用例：34 / 34
-    func levelOrder(_ root: TreeNode?) -> [Int] {
-        var results: [Int] = []
+    func levelOrder(_ root: TreeNode?) -> [[Int]] {
+        var results: [[Int]] = []
         var _root: TreeNode? = root
         if _root == nil {
             return []
         }
-        
+        var cur: [TreeNode?] = [_root]
+        var reverse: Bool = false
+        while !cur.isEmpty {
+            var nex: [TreeNode?] = []
+            var vals: [Int] = []
+            for node in cur.reversed() {
+                if let node = node {
+                    vals.append(node.val)
+                    if reverse {
+                        if let right = node.right {
+                            nex.append(right)
+                        }
+                        if let left = node.left {
+                            nex.append(left)
+                        }
+                    } else {
+                        if let left = node.left {
+                            nex.append(left)
+                        }
+                        if let right = node.right {
+                            nex.append(right)
+                        }
+                    }
+                }
+            }
+            reverse.toggle()
+            if !vals.isEmpty {
+                results.append(vals)
+            }
+            cur = nex
+        }
         
         return results
    }
