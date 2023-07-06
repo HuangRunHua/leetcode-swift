a913a43b2496243dd725740b512e131768c9c457
Huang Runhua <45262463+HuangRunHua@users.noreply.github.com>
2023-07-06 15:15:18 +0800

    Add code

--- 剑指Offer/剑指Offer64-求1+2+…+n.playground/Contents.swift
+++ 剑指Offer/剑指Offer64-求1+2+…+n.playground/Contents.swift
@@ -1,3 +1,31 @@
-import UIKit
+import Foundation
 
-var greeting = "Hello, playground"
+class Solution {
+    
+    var res = 0
+    
+    /// 执行用时：8 ms
+    /// 内存消耗：13.8 MB
+    func sumNumsSolution1(_ n: Int) -> Int {
+        (n > 1) && ((sumNumsSolution1(n - 1)) != 0)
+        res += n
+        return res
+    }
+    
+    func sumNumsTest(_ n: Int) -> Int {
+        var sum = 0
+        for i in 0...n {
+            sum += i
+        }
+        return sum
+    }
+    
+    /// 执行用时：0 ms, 在所有 Swift 提交中击败了 100.00% 的用户
+    /// 内存消耗：13.4 MB, 在所有 Swift 提交中击败了 87.10% 的用户
+    func sumNums(_ n: Int) -> Int {
+        return Array(1...n).reduce(0,+)
+    }
+}
+
+let solution = Solution()
+print(solution.sumNums(9) == solution.sumNumsTest(9))
