137e829a5fb9ffed9ab7037be82679cec39eb9cc
Huang Runhua <45262463+HuangRunHua@users.noreply.github.com>
2023-07-10 16:51:25 +0800

    update code

--- 剑指Offer/剑指Offer12-矩阵中的路径.playground/Contents.swift
+++ 剑指Offer/剑指Offer12-矩阵中的路径.playground/Contents.swift
@@ -1,5 +1,31 @@
 import Foundation
-
+// 剑指 Offer 12. 矩阵中的路径
+// 给定一个 m x n 二维字符网格 board 和一个字符串单词 word 。
+// 如果 word 存在于网格中，返回 true ；否则，返回 false 。
+//
+// 单词必须按照字母顺序，通过相邻的单元格内的字母构成，其中“相邻”单
+// 元格是那些水平相邻或垂直相邻的单元格。同一个单元格内的字母不允许被重复使用。
+//
+// 例如，在下面的 3×4 的矩阵中包含单词 "ABCCED"（单词中的字母已标出）。
+// 示例 1：
+// 输入：board = [["A","B","C","E"],
+//               ["S","F","C","S"],
+//               ["A","D","E","E"]],
+//      word = "ABCCED"
+// 输出：true
+//
+// 示例 2：
+// 输入：board = [["a","b"],
+//               ["c","d"]],
+//      word = "abcd"
+// 输出：false
+//
+// 提示：
+// m == board.length
+// n = board[i].length
+// 1 <= m, n <= 6
+// 1 <= word.length <= 15
+// board 和 word 仅由大小写英文字母组成
 class Solution {
     func existSolution1(_ board: [[Character]], _ word: String) -> Bool {
         if board.isEmpty { return false }
