a29aa4b67129239efe39d1d442e467d8f66c0e52
Huang Runhua <45262463+HuangRunHua@users.noreply.github.com>
2023-07-25 10:29:53 +0800

    添加`剑指 Offer 53 - I. 在排序数组中查找数字 I`解法3

--- 剑指Offer/剑指Offer53-I-在排序数组中查找数字 I.playground/Contents.swift
+++ 剑指Offer/剑指Offer53-I-在排序数组中查找数字 I.playground/Contents.swift
@@ -16,6 +16,21 @@ class Solution {
         }
         return freq
     }
+    /// 执行用时: 36 ms, 在所有 Swift 提交中击败了 85.56% 的用户
+    /// 内存消耗: 14.53 MB, 在所有 Swift 提交中击败了 66.67% 的用户
+    /// 通过测试用例: 88 / 88
+    func searchSolution2(_ nums: [Int], _ target: Int) -> Int {
+        var left: Int = 0
+        var right: Int = nums.count-1
+        while left <= right {
+            if nums[left] < target { left += 1 }
+            else if nums[left] > target { return 0 }
+            if nums[right] > target { right -= 1 }
+            else if nums[right] < target { return 0 }
+            if nums[left] == target && nums[right] == target { break }
+        }
+        return right - left + 1
+    }
     
     func helper(_ nums: [Int], _ target: Int, _ lower: Bool) -> Int {
         var start = 0
