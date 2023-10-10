ba689ce5f43c13aa3646383666b8b2d49ae3f842
Huang Runhua <45262463+HuangRunHua@users.noreply.github.com>
2023-10-10 13:24:49 +0800

    Add `面试题 05.03. 翻转数位`

---
+++ 其他杂题/面试题 05.03. 翻转数位.playground/Contents.swift
@@ -0,0 +1,120 @@
+import Foundation
+/// **面试题 05.03. 翻转数位**
+/// 给定一个32位整数 num，你可以将一个数位从0变为1。请编写一个程序，找出你能够获得的最长的一串1的长度。
+///
+/// **示例 1**
+/// 输入: num = 1775 (11011101111)
+/// 输出: 8
+class Solution {
+    func reverseBits(_ num: Int) -> Int {
+        let bits: [Int] = num2Bits(num)
+        var (left, right) = (0, 0)
+        var (lsum, rsum) = (0, 0)
+        var result: Int = 0
+        while right < bits.count {
+            let insertNum = bits[right]
+            right += 1
+            rsum += (1 - insertNum)
+
+            while rsum - lsum > 1 {
+                let popNum = bits[left]
+                left += 1
+                lsum += (1 - popNum)
+            }
+            result = max(result, right - left)
+        }
+        return result
+    }
+    
+    func num2Bits(_ num: Int) -> [Int] {
+        var _num: Int = num < 0 ? -num : num
+        var result: [Int] = Array(repeating: 0, count: 32)
+        var index: Int = 0
+        while _num > 0 {
+            result[index] = _num % 2
+            _num >>= 1
+            index += 1
+        }
+        if num < 0 {
+            for index in 0..<result.count {
+                result[index] = (result[index] == 0) ? 1 : 0
+            }
+            plusOne(&result)
+        }
+        return result
+    }
+    
+    func plusOne(_ nums: inout [Int]) {
+        var plus = false
+        for index in 0..<nums.count {
+            if index == 0 {
+                if nums[index] == 1 {
+                    plus = true
+                    nums[index] = 0
+                } else {
+                    nums[index] = 1
+                    break
+                }
+            } else {
+                if plus == true {
+                    if nums[index] == 1 {
+                        nums[index] = 0
+                    } else {
+                        nums[index] = 1
+                        plus = false
+                        break
+                    }
+                }
+            }
+        }
+    }
+}
+
+let solution = Solution()
+print(solution.reverseBits(1775))   // 8
+print(solution.reverseBits(7))      // 4
+print(solution.reverseBits(-1))     // 32
+print(solution.reverseBits(-4))     // 31
+
+func num2Bits(_ num: Int) -> [Int] {
+    var _num: Int = num < 0 ? -num : num
+    var result: [Int] = Array(repeating: 0, count: 32)
+    var index: Int = 0
+    while _num > 0 {
+        result[index] = _num % 2
+        _num >>= 1
+        index += 1
+    }
+    if num < 0 {
+        for index in 0..<result.count {
+            result[index] = (result[index] == 0) ? 1 : 0
+        }
+        plusOne(&result)
+    }
+    return result
+}
+
+func plusOne(_ nums: inout [Int]) {
+    var plus = false
+    for index in 0..<nums.count {
+        if index == 0 {
+            if nums[index] == 1 {
+                plus = true
+                nums[index] = 0
+            } else {
+                nums[index] = 1
+                break
+            }
+        } else {
+            if plus == true {
+                if nums[index] == 1 {
+                    nums[index] = 0
+                } else {
+                    nums[index] = 1
+                    plus = false
+                    break
+                }
+            }
+        }
+    }
+}
